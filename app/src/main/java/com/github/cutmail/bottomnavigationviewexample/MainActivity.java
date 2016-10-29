1ebdd357bbd01a07907886319e5c6c94619e2caa
cutmail <sunday.is.over@gmail.com>
2016-10-29 18:28:17 +0900

    first commit

---
+++ app/src/main/java/com/github/cutmail/bottomnavigationviewexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.github.cutmail.bottomnavigationviewexample;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d("MainActivity", item.getTitle().toString());
+                return true;
+            }
+        });
+    }
+}
